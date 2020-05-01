2e48cc7d4f5a475aab2f148ad61639c666c92abe
Sebastian-R0jas-696 <64318240+Sebastian-R0jas-696@users.noreply.github.com>
2020-05-01 18:40:44 -0500

    segundo intento

--- Caso_14.java
+++ Caso_14.java
@@ -1,9 +1,55 @@
-
+import java.util.Scanner;
+//Creado por Sebastian Rojas
 public class Caso_14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		String alumno = "",carrera = "", turno = "";
+		float monto, dsct, total, dolares;
+		
+		System.out.print("Alumno: ");
+		alumno = sc.nextLine();
+		System.out.print("Carrera: ");
+		carrera=sc.nextLine();
+		System.out.print("Turno: ");
+		turno=sc.nextLine();
+		
+		switch(carrera){
+		case "DS":
+			monto = 1500;
+			break;
+		case "RC":
+			monto = 1400;
+			break;
+		case "DG":
+			monto = 1300;
+			break;
+		default:
+			monto = 0;
+			break;			
+		}
+		
+		switch(turno){
+		case "Mañana":
+			dsct =(float)(monto * 0.1);
+			break;
+		case "Tarde":
+			dsct =(float)(monto * 0.2);
+		case "Noche":
+			dsct = (float)(monto * 0.15);
+		default:
+			dsct = 0;
+		}
+		
+		total = monto - dsct;
+		dolares = (float)(total / 3.33);
+		
+		System.out.println("Monto: "+ monto);
+		System.out.println("Turno: "+ turno);
+		System.out.println("Descuento: " + dsct);
+		System.out.println("Total a pagar: " + total);
+		System.out.println("Dolares: " + dolares);
 	}
 
 }
--- Caso_15.java
+++ Caso_15.java
@@ -1,8 +1,58 @@
-
+import java.util.Scanner;
+//Creado por Sebastian Rojas
 public class Caso_15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String cliente = "", producto = "", formapago = "";
+		float importe = 0, dsct = 0, cantidad = 0, precio = 0, imcremento = 0, total = 0;
+		
+		System.out.print("Nombre del Cliente: ");
+		cliente = sc.nextLine();
+		System.out.print("Nombre del Producto: ");
+		producto = sc.nextLine();
+		System.out.println("Cantidad: ");
+		cantidad = sc.nextFloat();
+		
+		switch (producto){
+		case "Tablet":
+			precio = 350;
+			break;
+		case "Laptop":
+			precio = 2300;
+			break;
+		case "Monitor":
+			precio = 850;
+			break;
+		case "Impresora":
+			precio = 680;
+			break;
+			default:
+				precio = 0;
+		}
+		
+		importe = precio * cantidad;
+		
+		System.out.print("Forma de pago: ");
+		formapago = sc.nextLine();
+		
+		switch (formapago) {
+		case "Contado":
+			dsct = (float)(importe * 0.05);
+			break;
+		case "Credito":
+			imcremento = (float)(importe * 012);
+			break;
+			default:
+				imcremento = 0;
+		}
+		total = (float)((precio * cantidad) + dsct - imcremento);
+		
+		System.out.println("Precio: " + precio);
+		System.out.println("Importe: " + importe);
+		System.out.println("Descuento: " + dsct);
+		System.out.println("Total a pagar: " + total);
+		
 
 	}
 
